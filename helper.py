75df18574b46310b8356a2468c17d8a9a73e6c88
Owen Ouyang <30837746+couyang24@users.noreply.github.com>
2021-01-17 21:46:20 -0500

    Update helper.py

--- helper.py
+++ helper.py
@@ -11,3 +11,12 @@ def rprint(func, lst):
         print("The Input dataset is: {}".format(item))
         print("The Output dataset is: {}".format(result_lst))
         print("********************************")
+
+def rprint2(func, lst1, lst2):
+    for index in range(len(lst1)):
+        result_lst = func(lst1[index], lst2[index])
+        sprint("Example {}:".format(index + 1))
+        print("********************************")
+        print("The Input dataset is: {}".format(lst1[index], lst2[index]))
+        print("The Output dataset is: {}".format(result_lst))
+        print("********************************")
