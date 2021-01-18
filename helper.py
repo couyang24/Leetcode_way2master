c3bf5f86c6c2dcf572a0c60296a93b79d179ac52
Owen Ouyang <30837746+couyang24@users.noreply.github.com>
2021-01-17 23:38:56 -0500

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,22 +1,32 @@
 from pyfiglet import Figlet
 from termcolor import cprint
+from icecream import ic
 
 sprint = lambda x: cprint(Figlet(font="standard").renderText(x), "red")
 
-def rprint(func, lst):
+def rprint(func, lst, outputs=[]):
     for index, item in enumerate(lst):
-        result_lst = func(item)
+        result = func(item)
         sprint("Example {}:".format(index + 1))
         print("********************************")
         print("The Input dataset is: {}".format(item))
-        print("The Output dataset is: {}".format(result_lst))
+        if outputs == []:
+            print("The Output dataset is: {}.".format(result))
+        else:
+            print("The Output dataset is: {} and the correct answer is {}.".format(result, outputs[index]))
+            assert result == outputs[index], "The results do not match"
         print("********************************")
 
-def rprint2(func, lst1, lst2):
+def rprint2(func, lst1, lst2, outputs=[]):
     for index in range(len(lst1)):
-        result_lst = func(lst1[index], lst2[index])
+        result = func(lst1[index], lst2[index])
         sprint("Example {}:".format(index + 1))
         print("********************************")
-        print("The Input dataset is: {}".format(lst1[index], lst2[index]))
-        print("The Output dataset is: {}".format(result_lst))
+        print("The Input dataset is: {} & {}".format(lst1[index], lst2[index]))
+        if outputs == []:
+            print("The Output dataset is: {}.".format(result))
+        else:
+            print("The Output dataset is: {} and the correct answer is {}.".format(result, outputs[index]))
+            assert result == outputs[index], "The results do not match"
         print("********************************")
+
